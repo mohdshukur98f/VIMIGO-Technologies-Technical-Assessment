0349cd0006f30132c2a80d8600db16fec1088ee2
mohdshukur98f <mohdshukur98f@gmail.com>
2022-07-30 20:18:03 +0800

    Installing storyline no 2.
    2. All data time format should display in d MMM Y hh:mm a format

---
+++ candidate_technical_assessment/lib/home.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<Home> {
+  late double screenHeight, screenWidth;
+  String server = "https://seriouslaa.com/vimigoTech/php";
+
+  List contactdata = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadcontact();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Vimigo'),
+          ),
+          body: GridView.count(
+            scrollDirection: Axis.vertical,
+            crossAxisCount: 1,
+            childAspectRatio: 5,
+            children: List.generate(
+                contactdata.length,
+                (index) => Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 2,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              contactdata[index]['user'],
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              contactdata[index]['phone'],
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              DateFormat('dd MMM yyyy hh:mm a').format(
+                                  DateTime.parse(
+                                      contactdata[index]['check_in'])),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
+          )),
+    );
+  }
+
+  void _loadcontact() async {
+    await http.post(Uri.parse(server + "/load_user.php"), body: {}).then((res) {
+      if (res.body == "nodata") {
+      } else {
+        setState(() {
+          var extractdata = json.decode(res.body);
+          contactdata = extractdata["contact"];
+          // cartquantity = widget.user.quantity;
+        });
+      }
+    }).catchError((err) {});
+  }
+}
--- candidate_technical_assessment/lib/main.dart
+++ candidate_technical_assessment/lib/main.dart
@@ -1,54 +1,18 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
+import 'home.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late double screenHeight, screenWidth;
-  String server = "https://seriouslaa.com/vimigoTech/php";
-
-  List contactdata = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadcontact();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Text(contactdata[1]["user"]),
-        ),
-      ),
+      title: 'Vamigo Technologies',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const Home(),
     );
-  }
-
-  void _loadcontact() async {
-    await http.post(Uri.parse(server + "/load_user.php"), body: {}).then((res) {
-      if (res.body == "nodata") {
-      } else {
-        setState(() {
-          var extractdata = json.decode(res.body);
-          contactdata = extractdata["contact"];
-          // cartquantity = widget.user.quantity;
-        });
-      }
-    }).catchError((err) {});
   }
 }
